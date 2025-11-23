06798490787891175320799eb7764b851a070122
Anderson Araujo <161385114+andeAraujo@users.noreply.github.com>
2025-11-23 00:09:47 -0400

    update index and style

---
+++ asset/js/main.js
@@ -0,0 +1,7 @@
+// assets/js/main.js
+
+// Function to toggle the navigation menu
+function toggleMenu() {
+    const menu = document.getElementById('navMenu');
+    menu.classList.toggle('active');
+}
